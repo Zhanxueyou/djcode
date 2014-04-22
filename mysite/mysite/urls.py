9525b54a3af6db73c6e3dffdaab3dd2e4c877a32
Zhanxueyou <zhanxueyou1@gmail.com>
2014-04-22 18:38:47 +0800

    Revert "chapter3 动态页面基础完成"
    This reverts commit fc2daf9f27204540f6f86dfcd8da0ba2de3eca04.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     (r'^time/$', current_datetime),
-    (r'^time/plus/(\d+)/$', hours_ahead),   # 这里需要注意，调用哪个参数的位置的模式匹配必须有括号
-    #(r'^time/plus/\d+/$', hours_ahead),    # 这样写是不对的，会有错误产生
+
     #url(r'^admin/', include(admin.site.urls)),
 )
