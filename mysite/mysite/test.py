e84afe4ff4a3e0f0c6f1828a8957a89719e4bf98
Zhanxueyou <zhanxueyou1@gmail.com>
2015-02-27 15:40:55 +0800

    commit test

--- mysite/mysite/test.py
+++ mysite/mysite/test.py
@@ -1,4 +1,4 @@
 __author__ = 'Zhanxueyou'
 def application(env, start_response):
     start_response('200 OK!',[('Content_Type','text/html')])
-    return "uWSGI Testing OK!!!"
+    return "uWSGI Testing OK!!!!!!"
